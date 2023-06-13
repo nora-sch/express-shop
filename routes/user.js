a6aa1077d90e356947adaaff2d27dc8259b912a0
nora-sch <nora.schumann.development@gmail.com>
2023-06-13 16:54:39 +0200

    modify firstname for one user

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,10 @@ const dbConnection = require("../database/connection");
 
 const getAll = "SELECT * FROM users";
 const getOne = "SELECT * FROM users WHERE id=?";
-const createOne = "INSERT INTO users (first_name, last_name, email, created_at, updated_at, is_admin) VALUES(?, ?, ?, ?, ?, ?)";
+const createOne =
+  "INSERT INTO users (first_name, last_name, email, created_at, updated_at, is_admin) VALUES(?, ?, ?, ?, ?, ?)";
+const updateFirstnameOne =
+  "UPDATE users SET first_name = ?, updated_at = ? WHERE id = ?";
 
 const dateToday = new Date().toISOString().slice(0, 10);
 
@@ -19,20 +22,24 @@ router.get("/", (req, res) => {
   });
 });
 router.post("/", (req, res) => {
-    const { firstName, lastName, email} = req.body;
-    // {
-    //     "firstName":"Nora", 
-    //     "lastName":"Sumane", 
-    //     "email":"norah@inbox.lv"
-    // }
-    dbConnection.query(createOne, [firstName, lastName, email, dateToday, dateToday, false], (err, result, fields) => {
+  const { firstName, lastName, email } = req.body;
+  // {
+  //     "firstName":"Nora",
+  //     "lastName":"Sumane",
+  //     "email":"norah@inbox.lv"
+  // }
+  dbConnection.query(
+    createOne,
+    [firstName, lastName, email, dateToday, dateToday, false],
+    (err, result, fields) => {
       if (!err) {
         res.location(`/${result.insertId}`).sendStatus(201);
       } else {
         res.status(500).send("Error saving the user");
       }
-    });
-  });
+    }
+  );
+});
 
 // ROUTE "/:id"
 router.get("/:id", (req, res) => {
@@ -47,6 +54,28 @@ router.get("/:id", (req, res) => {
       res.status(500).send("Error saving the user");
     }
   });
+});
+router.put("/:id", (req, res) => {
+  // {
+  //     "firstName":"Alice",
+  // }
+  const id = parseInt(req.params.id);
+  const { firstName } = req.body;
+  dbConnection.query(
+    updateFirstnameOne,
+    [firstName, dateToday, id],
+    (err, result, fields) => {
+      if (!err) {
+        if (result.affectedRows === 0) {
+          res.status(404).send("Not Found");
+        } else {
+          res.sendStatus(204);
+        }
+      } else {
+        res.status(500).send("Error editing the user");
+      }
+    }
+  );
 });
 
 module.exports = router;
