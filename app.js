5145af1cfa97dd6f31b93d1ab8a05077d38e5b3b
nora-sch <nora.schumann.development@gmail.com>
2023-06-13 16:35:15 +0200

    post one user

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
+app.use(express.json()); 
 const seedRouter = require("./routes/seed");
 const userRouter = require("./routes/user");
 
--- routes/user.js
+++ routes/user.js
@@ -2,37 +2,51 @@ const express = require("express");
 const router = express.Router();
 const dbConnection = require("../database/connection");
 
-const getAll = 'SELECT * FROM users'
-const getOne = 'SELECT * FROM users WHERE id=?'
+const getAll = "SELECT * FROM users";
+const getOne = "SELECT * FROM users WHERE id=?";
+const createOne = "INSERT INTO users (first_name, last_name, email, created_at, updated_at, is_admin) VALUES(?, ?, ?, ?, ?, ?)";
+
+const dateToday = new Date().toISOString().slice(0, 10);
 
 // ROUTE "/"
 router.get("/", (req, res) => {
-     dbConnection.query(   
-        getAll, (err, result, fields) => {
-        if (!err) {
-           res.status(200).json(result);
-        } else {
-          res.status(500).send("Error saving the user");
-        }
+  dbConnection.query(getAll, (err, result, fields) => {
+    if (!err) {
+      res.status(200).json(result);
+    } else {
+      res.status(500).send("Error saving the user");
+    }
+  });
+});
+router.post("/", (req, res) => {
+    const { firstName, lastName, email} = req.body;
+    // {
+    //     "firstName":"Nora", 
+    //     "lastName":"Sumane", 
+    //     "email":"norah@inbox.lv"
+    // }
+    dbConnection.query(createOne, [firstName, lastName, email, dateToday, dateToday, false], (err, result, fields) => {
+      if (!err) {
+        res.location(`/${result.insertId}`).sendStatus(201);
+      } else {
+        res.status(500).send("Error saving the user");
       }
-    );
+    });
   });
 
 // ROUTE "/:id"
-  router.get("/:id", (req, res) => {
-    dbConnection.query(   
-       getOne, [req.params.id], (err, result, fields) => {
-       if (!err) {
-        if (result.length > 0) {
-            res.status(200).json(result);
-          } else {
-            res.status(400).json({message:'USER NOT FOUND'});
-          }
-       } else {
-         res.status(500).send("Error saving the user");
-       }
-     }
-   );
- });
+router.get("/:id", (req, res) => {
+  dbConnection.query(getOne, [req.params.id], (err, result, fields) => {
+    if (!err) {
+      if (result.length > 0) {
+        res.status(200).json(result);
+      } else {
+        res.status(400).json({ message: "USER NOT FOUND" });
+      }
+    } else {
+      res.status(500).send("Error saving the user");
+    }
+  });
+});
 
 module.exports = router;
